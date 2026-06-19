fcd1b5783687af4fb1456349c6503f5f8baa8514
Julia Peghini <juliapeghini@gmail.com>
2026-06-19 11:48:38 -0300

    feat:log de progresso e elitura de CSV (closes #9, closes #11, closes #12, closes #13)

--- py/enviar_email.py
+++ py/enviar_email.py
@@ -1,20 +1,48 @@
 import smtplib
+import pandas as pd
+import time 
+
 from config import EMAIL, SENHA
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+tabela = pd.read_csv("planilhas/teste.csv")
+
 servidor = smtplib.SMTP_SSL("smtp.gmail.com", 465)
 servidor.login(EMAIL, SENHA)
+contador = 0
+#deixar isso aqui em cima sempre
 
-msg = MIMEMultipart()
+try:
+    with open("log.txt", "r") as arquivo_log:
+        emails_log = arquivo_log.read().splitlines()
+except:
+    emails_log = []
 
-msg['From'] = EMAIL
-msg['To'] = EMAIL
-msg['Subject'] = "Testeeeeee"
+#essa parte tá enviando o email, então pode ficar por ultimo
+for indice, linha in tabela.iterrows():
+    if linha["email"] in emails_log:
+        print(f'Email para {linha["email"]} já foi enviado anteriormente. Pulando')
+        continue
 
-corpo = "email de testeee, oremos, omnia in bonum"
-msg.attach(MIMEText(corpo,'plain'))
+    msg = MIMEMultipart()
+    msg['From'] = EMAIL
+    msg['To'] = linha['email']
+    msg['Subject'] = "Testeeeeee"
+    corpo = "email de testeee, oremos, omnia in bonum"
+    msg.attach(MIMEText(corpo,'plain'))
 
-servidor.sendmail(EMAIL, EMAIL, msg.as_string())
+    servidor.sendmail(EMAIL, linha['email'], msg.as_string())
+
+    with open("log.txt", "a") as arquivo_log:
+            arquivo_log.write(linha["email"] + "\n")
+
+    print(f"Email enviado para: {linha['email']}")
+    contador +=1
+
+    if contador %480 ==0:
+        print("480 emails enviados. Pausa de 1h")
+        time.sleep(3600)  # 1hora
+
 servidor.quit()
-print("Email enviado com sucesso!")
+print("Emails enviados com sucesso!")
